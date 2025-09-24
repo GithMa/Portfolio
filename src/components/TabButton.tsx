a6d94b9a939b4d30e0f18013d155a3cf84d3633e
Githan Mayurathan <gith@n.mayurathan@gmail.com>
2025-09-24 18:29:51 +0200

    Add all files

--- src/components/TabButton.tsx
+++ src/components/TabButton.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 
-const TabButton = ({ active, selectTab, children }) => {
+type TabButtonProps = {
+  active: boolean;
+  selectTab: () => void;
+  children: React.ReactNode;
+};
+
+const TabButton = ({ active, selectTab, children }: TabButtonProps) => {
   const buttonClasses = active
     ? "text-white border-b-3 border-[#597081]"
     : "text-[#adb7be]";
